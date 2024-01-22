7822d358e10aa2891fa085f81f5ae61de1917c10
nisipos <nisipos.daemon@gmail.com>
2024-01-22 12:14:24 +0800

    misc: compile css and js file

---
+++ app/includes/css.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
+<link href="https://cdn.jsdelivr.net/gh/eliyantosarage/font-awesome-pro@main/fontawesome-pro-6.5.1-web/css/all.min.css" rel="stylesheet">
+<link rel="stylesheet" href="./src/css/app.css">
