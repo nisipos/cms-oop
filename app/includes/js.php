0baa6aad5f9d44d856b9bd9d53c2bc8fb4456600
nisipos <nisipos.daemon@gmail.com>
2024-01-22 16:42:01 +0800

    misc: add sweetalert library

--- app/includes/js.php
+++ app/includes/js.php
@@ -1,4 +1,5 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="./src/js/app.js"></script>
